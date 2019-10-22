3dbf07963d167a1a204c3da4f1ffb7a8ee5e744a
yangtongkuan <yangtongkuan@qq.com>
2019-10-22 18:02:01 +0800

    ### 用户rpc api 模块

---
+++ cos-mall-user-api/src/main/java/com/cos/dto/user/UserInfoDto.java
@@ -0,0 +1,19 @@
+package com.cos.dto.user;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @User: @Created by yangtk
+ * @Date: @Date 2019/10/22 17:58
+ * @Classname: UserInfoDto
+ * @To change this template use File | Settings | File Templates.
+ */
+@Data
+@Accessors(chain = true)
+public class UserInfoDto implements Serializable {
+}
---
+++ cos-mall-user-api/src/main/java/com/cos/service/user/UserService.java
@@ -0,0 +1,12 @@
+package com.cos.service.user;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @User: @Created by yangtk
+ * @Date: @Date 2019/10/22 17:56
+ * @Classname: UserService
+ * @To change this template use File | Settings | File Templates.
+ */
+public interface UserService {
+}
