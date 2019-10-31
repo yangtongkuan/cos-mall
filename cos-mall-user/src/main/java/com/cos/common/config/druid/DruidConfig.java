20459b359beeed2b519e65efd1a9963cedc89032
yangtongkuan <yangtongkuan@qq.com>
2019-10-31 09:18:35 +0800

    ### 使用dubbo2.7.2的架包

--- cos-mall-user/src/main/java/com/cos/common/config/druid/DruidConfig.java
+++ cos-mall-user/src/main/java/com/cos/common/config/druid/DruidConfig.java
@@ -1,4 +1,4 @@
-package com.cos.commom.config.druid;
+package com.cos.common.config.druid;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
--- cos-mall-user/src/main/java/com/cos/common/config/dubbo/DubboConfig.java
+++ cos-mall-user/src/main/java/com/cos/common/config/dubbo/DubboConfig.java
@@ -1,4 +1,4 @@
-package com.cos.commom.config.druid;
+package com.cos.common.config.dubbo;
 
 import org.apache.dubbo.config.MetadataReportConfig;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +10,7 @@ public class DubboConfig {
     @Bean
     public MetadataReportConfig metadataReportConfig() {
         MetadataReportConfig metadataReportConfig = new MetadataReportConfig();
-        metadataReportConfig.setAddress("zookeeper://118.25.83.43:2181");
+        metadataReportConfig.setAddress("zookeeper://127.0.0.1:2181");
         return metadataReportConfig;
     }
 
