ecb8ac6bcac49ecfb5dfdd1751f956caf12398c0
yangtongkuan <yangtongkuan@qq.com>
2019-10-31 18:10:45 +0800

    ### 配置文件配置类型实现数据元注册

--- cos-mall-user/src/main/java/com/cos/common/config/dubbo/DubboConfig.java
+++ cos-mall-user/src/main/java/com/cos/common/config/dubbo/DubboConfig.java
@@ -1,16 +1,21 @@
 package com.cos.common.config.dubbo;
 
 import org.apache.dubbo.config.MetadataReportConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DubboConfig {
 
+    @Value("${dubbo.metadata.address}")
+    private String metadataAddress;
+
     @Bean
     public MetadataReportConfig metadataReportConfig() {
         MetadataReportConfig metadataReportConfig = new MetadataReportConfig();
-        metadataReportConfig.setAddress("zookeeper://127.0.0.1:2181");
+//        metadataReportConfig.setAddress("zookeeper://118.25.83.43:2181");
+        metadataReportConfig.setAddress(metadataAddress);
         return metadataReportConfig;
     }
 
